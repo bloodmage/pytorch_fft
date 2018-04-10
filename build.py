022b488e9edc9db50d8d4deecf412ce307657367
Edgar Y. Walker <edgar.walker@gmail.com>
2018-04-09 21:25:00 -0500

    Always compile with CUDA

--- build.py
+++ build.py
@@ -1,5 +1,4 @@
 import os
-import torch
 from torch.utils.ffi import create_extension
 
 this_file = os.path.dirname(__file__)
@@ -9,12 +8,11 @@ headers = []
 defines = []
 with_cuda = False
 
-if torch.cuda.is_available():
-    print('Including CUDA code.')
-    sources += ['pytorch_fft/src/th_fft_cuda.c']
-    headers += ['pytorch_fft/src/th_fft_cuda.h']
-    defines += [('WITH_CUDA', None)]
-    with_cuda = True
+print('Including CUDA code.')
+sources += ['pytorch_fft/src/th_fft_cuda.c']
+headers += ['pytorch_fft/src/th_fft_cuda.h']
+defines += [('WITH_CUDA', None)]
+with_cuda = True
 
 ffi = create_extension(
     'pytorch_fft._ext.th_fft',
